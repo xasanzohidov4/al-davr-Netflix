95c58fd9182d1abea727077e3e60f1f325cf2c37
xasanzohidov4 <xasanzohidov4@gmail.com>
2024-11-13 16:16:58 +0500

    Update Netflix.js

--- Netflix.js
+++ Netflix.js
@@ -1,4 +1,4 @@
-<script>
+
         const API_KEY = '3fd2be6f0c70a2a598f084ddfb75487c';
         const BASE_URL = 'https://api.themoviedb.org/3';
         const IMG_URL = 'https://image.tmdb.org/t/p/w500';
@@ -122,4 +122,4 @@
     
         // Initial load
         fetchMovies();
-      </script>
+  
